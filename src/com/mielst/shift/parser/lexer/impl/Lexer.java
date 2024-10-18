543c8907422460cb62597ea5cd742547ef3f7caf
mielst <iliaforhome@gmail.com>
2024-10-18 11:05:24 +0300

    Edited Lexer,java

--- src/com/mielst/shift/parser/lexer/impl/Lexer.java
+++ src/com/mielst/shift/parser/lexer/impl/Lexer.java
@@ -6,10 +6,38 @@ import com.mielst.shift.parser.lexer.ILexer;
 import com.mielst.shift.parser.lexer.token.Token;
 
 public final class Lexer implements ILexer {
-    @Override
+
+    public static final String operators = "+-*/%()[]{}@#!<>~|.,:;=&^?";
+    public static final List<String> keywords = {
+        // TODO
+    };
+
     public List<Token> tokenize(String input) {
         List<Token> tokens = null;
-
+        int length = input.length;
+        for (int i; i <= length; i++) {
+            char current = input[i];
+            if (current == '"') tokenizeText();
+            else if (current == '\'') tokenizeChar();
+            else if (Character.isDigit(current)) tokenizeNumber();
+            else if (operators.indexOf(current) != -1) tokenizeOperator();
+        }
         return tokens;
+    }
+
+    public void tokenizeText() {
+        // TODO
+    }
+
+    public void tokenizeChar() {
+
+    }
+
+    public void tokenizeNumber() {
+
+    }
+
+    public void tokenizeOperator()  {
+
     }
 }
